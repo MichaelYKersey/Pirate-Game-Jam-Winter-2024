d7313f09743dd350f4372416f763ed9fe77e01fe
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2024-01-12 22:05:41 -0800

    Make Basic Start Menu and Camera Pan

---
+++ Camera2D.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var mouseCords = Vector2(0, 0);
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var newMouseCords = get_viewport().get_mouse_position();
+	if Input.is_action_pressed("pan") :
+		position += mouseCords -newMouseCords;
+	mouseCords = newMouseCords;
+	pass
