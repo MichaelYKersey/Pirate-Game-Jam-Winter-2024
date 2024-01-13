971c8ac8d3edd1d451c0fec5d3ef977e7bdc4100
MichaelYKersey <144640226+MichaelYKersey@users.noreply.github.com>
2024-01-12 22:44:13 -0800

    Add Camera Zoom with Clamp

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,16 +1,23 @@
 extends Camera2D
 
-var mouseCords = Vector2(0, 0);
+var mouseCords : Vector2= Vector2(0, 0);
+const minZoom : float = 1.0/4;
+const maxZoom : float = 4;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var newMouseCords = get_viewport().get_mouse_position();
 	if Input.is_action_pressed("pan") :
-		position += mouseCords -newMouseCords;
+		position += (mouseCords -newMouseCords) / zoom;
 	mouseCords = newMouseCords;
 	pass
+
+func _input(event):
+	if event.is_action_pressed("zoom_in") and zoom.length() < maxZoom:
+		zoom *= 1.25;
+	if event.is_action_pressed("zoom_out") and zoom.length() > minZoom:
+		zoom /= 1.25;
